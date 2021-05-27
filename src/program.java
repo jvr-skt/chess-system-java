ea3475e39885f525820b017507009f23430bfeda
Jean Victor <jvrferreira96@gmail.com>
2021-05-27 15:00:27 -0300

    First class position:

--- src/program.java
+++ src/program.java
@@ -1,3 +1,6 @@
+
+import boardgame.Position;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -14,7 +17,9 @@ public class program {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Position p = new Position(3, 5);
+        System.out.println(p);
+        
     }
     
 }
